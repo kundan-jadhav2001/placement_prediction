e75b922f5881d23c8d326f0c9d8eee1ae1215443
Kiedy <kundanjadhav2001@gamil.com>
2024-02-13 22:00:14 +0530

    made files django compatible

--- model/urls.py
+++ model/urls.py
@@ -3,6 +3,9 @@ from model import views
 
 urlpatterns = [
     path('',views.home),
-    path('signinup',views.signinup)
-    
+    path('signinup',views.signinup),
+    path('contact',views.contact),
+    path('course',views.course),
+    path('blog',views.blog),
+    path('about',views.about),
 ]
--- model/views.py
+++ model/views.py
@@ -7,3 +7,14 @@ def home(request):
 def signinup(request):
     return render(request, "index.html")
 
+def contact(request):
+    return render(request, "index1.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def course(request):
+    return render(request, "course.html")
